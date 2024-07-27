3bf0264e0e3a189fbdff6f424a232ba2fc77adae
ususio <steppingwinds@gmail.com>
2024-07-28 00:22:19 +0900

    翻訳追加

--- src/i18n.ts
+++ src/i18n.ts
@@ -6,29 +6,225 @@ i18n
   .init({
     resources: {
       en: {
-        translation: {
-          metaDescription: "Text-based adventure game where you maintain a robot, 'Repair the Cocoro'.",
+        translation: { metaDescription: "A text-based adventure game where you maintain a robot while controlling it, titled 'Repair the Heart'.",
           welcome: "Welcome to React and i18next",
-          title: "Repair the cocoro",
-          sample1: "Amazing! It's the first time a human technician is doing maintenance for me!",
-          sample2: "Currently in maintenance mode. If you want to interrupt, please use the console.",
+          title: "Repair the Heart",
+          sample1: "Wow!\nIt's my first time being maintained by a human engineer!",
+          sample2: "Currently in maintenance mode.\nTo interrupt, please operate from the console.",
           sample3: "Opening the abdominal hatch.",
-          sample4: "Um... My stomach is still open...",
-
-          textA_1: "Hello! I'm here for maintenance today...",
-          textA_2: "Are you... a human?",
-          textA_3: "Amazing! It's the first time a human technician is doing maintenance for me!",
-
-          choiceB: "Please tell me about yourself.",
-
-          textB_1: "I'm Rocoro! My formal model is roc-o-4902!",
-          textB_2: "I'm in my 8th year of operation, and my role is a childcare robot.",
-          textB_3: "There are no notable defects or omissions during manufacturing or operation.",
-
-
-
-          cannotDeleteError: "Error: The DELETE command involves destructive changes! Inappropriate use is restricted.",
+          sample4: "Um...\nMy stomach is still open, you know...?",
         
+          textA_1: "Hello!\nThank you for the maintenance today as well...",
+          textA_2: "...Wait, are you a human?",
+          textA_3: "Wow!\nIt's my first time being maintained by a human engineer!",
+          
+          choiceB: "Please tell me about yourself.",
+        
+          textB_1: "I'm Rocoro! My official model number is roc-o-4902!",
+          textB_2: "I'm in my 8th year of manufacture, and my role is a childcare robot.",
+          textB_3: "There are no notable defects or shortcomings in my manufacturing or operation.",
+        
+          choiceC: "What brings you here today?",
+        
+          textC_1: "I've been having a problem lately...",
+          textC_2: "No matter how much I charge, I get hungry again quickly.",
+          textC_3: "Even though I charged for 8 hours yesterday, I'm already starving...",
+          textC_4: "I keep charging whenever I have free time, but it's still not enough.",
+          textC_5: "At this rate, I won't be able to work as a childcare robot, or even live a normal life.",
+          textC_6: "Doctor, can you help me?",
+        
+          textD_1: "Initiating diagnostic program.",
+          textD_2: "To avoid the risk of malfunction,\nplease maintain a distance of 3 meters from this unit.",
+          textD_3: "Scanning the status of the computational system.\n\n...Status: All green.",
+          textD_4: "Scanning the status of sensor system, sensory system, and feedback system.\n\n...Status: All green.",
+          textD_5: "Scanning the status of the drive system.\n\nWarning! Voltage level is below the acceptable range.",
+          textD_6: "Error! Battery life has expired.\nBattery replacement is necessary.\nPlease open the hatch.",
+        
+          textD_7: "...Doctor, how was it?",
+        
+          textD_7_2: "...No matter how many times you diagnose, the result won't change.",
+        
+          choiceE: "It seems that the battery needs to be replaced.",
+        
+          textE_1: "Ah, so it was the battery after all...",
+          textE_2: "I see.\nCan you open my stomach hatch and take it out?",
+          textE_3: "I can't replace the battery myself.",
+        
+          textF_1: "Opening the abdominal hatch.",
+          textF_1_a: "Closing the abdominal hatch.",
+        
+          textF_2: "Error! Operation rejected.\nThis operation cannot be executed as it contradicts an order issued to this unit.",
+        
+          textF_3: "Huh, is the replacement finished?",
+          textF_4: "...Wait, my stomach is still open...",
+        
+          choiceG: "The battery replacement was rejected.",
+        
+          textG_1: "Oh, really? Why?",
+        
+          choiceH_1: "It seems to violate another order...",
+          choiceH_2: "Do you remember receiving any orders?",
+        
+          textH_1: "Huh? Ah... um...",
+          textH_2: "I'm sorry, I should have told you from the beginning.",
+          textH_3: "I don't remember anything from the past.",
+        
+          choiceI: "Why don't you have any memories?",
+        
+          textI_1: "Doctor, do you know about the 'Iron Rule of Childcare Robots'?",
+          textI_2: "It states, 'Robots must not interfere with humans whose contracts have expired unless requested by the human.'",
+          textI_3: "Even if I remember and want to meet them,\nI'm prohibited from visiting them on my own.",
+          textI_4: "Additionally, to avoid violating this rule,\nrelated memories are sealed.",
+          textI_5: "What's wrong? Are you sympathizing with me?",
+          textI_6: "That's not good. Doctor, as a maintenance engineer, you should know that robots' sensibilities are different from humans, right?",
+          textI_7: "Oh, but don't worry!\nEven though I said the memories are sealed, they're just compressed and stored in storage!",
+          textI_8: "If you load and decompress them, you can check them.",
+        
+          pleaseEnterPassword: "A password is required to check this content.",
+          enterPassword: "Please enter the password.",
+          passwordIncorrect: "Incorrect password.",
+          passwordCorrect: "Authentication successful. Lock has been released.",
+        
+          textJ_1: "Ah, I locked it, didn't I.",
+          textJ_2: "I always put a password on important memories so I don't forget them!",
+          textJ_3: "So...",
+          textJ_4: "Um...",
+          textJ_5: "...",
+          textJ_6: "I forgot the password...",
+          textJ_7: "But wait! I think it's okay!",
+          textJ_8: "I'm sure I wrote it down somewhere nearby so I could check it quickly...",
+        
+          textK_1: "What are you going to do by removing my arm?",
+          textK_2: "There's no way a password would be in a place like this...",
+          textK_3: "...Something's written here.\n'4902'...?",
+          textK_4: "Huh, no way.\n4902 is my model number.",
+          textK_5: "I wouldn't use my own model number as a password and then forget it, right?",
+          textK_6: "...Right?",
+          textK_7: "No way...",
+          textK_8: "...Anyway, I'll take my arm back...",
+          textK_9: "There we go.",
+        
+          memory1_Start: "Loading compressed memory data 1.",
+          memory1_1: "Today it was raining, so I drew pictures with Chii.",
+          memory1_2: "Chii used lots of colors to draw my face!",
+          memory1_3: "I tried drawing Chii too, but Chii made a funny face at me. I wonder why?",
+        
+          memory2_Start: "Loading compressed memory data 2.",
+          memory2_1: "Today I played soccer with Chii and Chii's friend Rin!",
+          memory2_2: "The ball went into the neighbor's garden, and when Chii and I went to apologize, the neighbor's grandmother gave us snacks.",
+          memory2_3: "Rin, the grandmother, and the snacks became new friends!",
+        
+          memory3_Start: "Loading compressed memory data 3.",
+          memory3_1: "Today, Chii gave me a present.\nShe said it was to celebrate one year since I came to this house.",
+          memory3_2: "The present was a brand new battery.",
+          memory3_3: "Chii said this to me:",
+          memory3_4: "To my beloved, beloved Rocoro.\nThank you for always playing with me!",
+          memory3_5: "I'm giving you the battery you like.\nDon't ever take it off, and treasure it, okay?",
+        
+          memory_Finish: "That concludes the compressed memory data.",
+        
+          textL_1: "Ah.",
+          textL_2: "I remember...",
+          textL_3: "This memory is from the Harumi household where I used to live.",
+          textL_4: "The girl in the memory is Harumi Chii...\nThe girl who cared for me the most.",
+          textL_5: "This battery is a present Chii gave me.",
+          textL_6: "At that time, I interpreted what Chii said to me...",
+          textL_7: "'Don't ever take it off, and treasure it' as an order to me,\nand I saved it so I wouldn't forget.",
+          
+          textL_8: "...",
+          textL_9: "If I could meet Chii again, she would surely cancel the order...",
+          textL_10: "But... I can't meet Chii.",
+          textL_11: "The iron rule of childcare robots.\n'Robots must not interfere with humans whose contracts have expired unless requested by the human'.",
+          textL_12: "I'm... no longer Chii's childcare robot.",
+          textL_13: "I'm not allowed to go see Chii.",
+          textL_14: "Doctor, what... what should I do?",
+        
+          textM_0: "DELETE command executed.\nTarget: Childcare robot rules.",
+          choiceM_1: "I've deleted the rules that were binding you.",
+          textM_1: "...Huh?",
+          choiceM_2: "Now you can go see Chii.",
+          textM_2: "...Isn't it impossible to delete them arbitrarily?",
+          choiceM_3: "It's usually not allowed, but this is special.",
+          choiceM_4: "Please keep it a secret.",
+          textM_3: "Yeah... Th-thank you!",
+          textM_4: "I see, now I can meet her... I can meet Chii again!",
+          textM_5: "Oh, hehe, what should I do... I have so many things I want to talk about!",
+          choiceM_5: "This is all I can do. The rest is up to Chii.",
+          textM_6: "Yeah, you're right!\nI'll have Chii replace my battery with a new one!",
+          textM_7: "Thank you, human doctor!",
+        
+          endingA_0: "...A few days later.",
+          endingA_1: "-- Doctor, how are you?",
+          endingA_2: "Thanks to you deleting the rules that day,\nI was able to visit the Harumi household again.",
+          endingA_3: "There, I had Chii cancel the order,\nand she replaced my battery...",
+          endingA_4: "And what's more, I'm allowed to\nlive at the Harumi house again!",
+          endingA_5: "Chii doesn't need childcare anymore, but...\nshe says she just couldn't forget about me.",
+          endingA_6: "Now I'm not a childcare robot anymore,\nbut Chii's home tutor robot.",
+          endingA_7: "Doctor.",
+          endingA_8: "I'm so happy that I met you and was able to come back to this house.",
+          endingA_9: "From Rocoro.",
+          
+          textN_0: "DELETE command executed.\nTarget: Order from previous owner 'Harumi Chii'.",
+          choiceN_1: "I've deleted the order that was binding you.",
+          textN_1: "...Huh?",
+          choiceN_2: "Now we can replace your battery.",
+          textN_2: "...Why...?\nIsn't it impossible to delete them arbitrarily?",
+          choiceN_3: "It's usually not allowed, but this is special.",
+          choiceN_4: "Please keep it a secret.",
+          textN_4: "Yeah... Th-thank you!",
+          textN_5: "Then... please replace my battery.",
+        
+          textN_wait: "......",
+        
+          textN_6: "...I wonder if Chii will forgive me for removing the battery on my own.",
+          choiceN_6: "Was the Chii you knew someone who would get angry over something like this?",
+          textN_7: "...No, she wasn't like that.",
+          choiceN_7: "I'm sure Chii would want you to work as a childcare robot too.",
+          textN_8: "...You're right!",
+          textN_9: "Thank you, human doctor!\nI'll try my best once again!",
+        
+          endingB_0: "...A few days later.",
+          endingB_1: "-- Doctor, how are you?",
+          endingB_2: "Since you replaced my battery that day, my body has been feeling really good.",
+          endingB_3: "I wanted to thank you, so I sent you this letter.",
+          endingB_4: "And there's one more thing that made me happy...",
+          endingB_5: "I found a new family who will hire me!",
+          endingB_6: "The child in my new home is a 3-year-old boy.\nHe's energetic and playful, and I'm looking forward to playing with him!",
+          endingB_7: "Doctor.",
+          endingB_8: "I'll keep my memories of Chii safely stored in my memory, and I'll keep moving forward positively.",
+          endingB_9: "After all, I'm a childcare robot!",
+          endingB_10: "From Rocoro.",
+        
+          textO_1: "Warning! A dangerous operation has been selected.",
+          textO_2: "This operation will reset all parameters of this unit to factory settings.\nLost data cannot be restored.",
+          textO_3: "Are you sure you want to proceed?",
+          choiceO_1: "Execute",
+          choiceO_2: "Cancel",
+          
+          textP_1: "20% complete...",
+          textP_2: "66% complete...",
+          textP_3: "92% complete...",
+          textP_4: "99% complete......",
+        
+          textP_5: "Nice to meet you! I'm the childcare robot, roc-o-4902.\nFirst, please set my name!",
+        
+          textArm_1: "Removing arm parts.",
+          textArm_2: "...What are you going to do with my arm?\nIf you're not using it, put it back, okay?",
+          textArm_3: "...Mm.\nThanks for putting it back.",
+        
+          textLeg_1: "Removing leg parts.",
+          textLeg_2: "Hey~\nIf you remove my legs, I won't be able to move.",
+          textLeg_3: "...That's right,\njust push it in until you hear a click.",
+        
+          hatchClosedError: "Error: Cannot replace battery because the abdominal hatch is closed.",
+          hatchAlreadyOpenedError: "Error: The abdominal hatch is already open.",
+          hatchAlreadyClosedError: "Error: The abdominal hatch is already closed.",
+          cannotCommandError: "Error: Cannot execute due to insufficient permissions.",
+          invalidArgsError: "Error: Invalid input command. Please enter the correct arguments.",
+          cannotDeleteError: "Error: The DELETE command involves destructive changes! Its use is restricted in inappropriate situations.",
+          cannotDisturbError: "Error: This operation is no longer necessary for Rocoro.",
+          cannotFindRobotError: "Error: The target robot was not found. Please register the robot.",
+          
           docs0_title: "Robot Operation Manual",
           docs0_desc: "Press F12 to open the console and enter the following commands:",
           docs1_title: "CHECK",
@@ -280,7 +476,7 @@ i18n
           docs4_title: "読み出す（LOAD）",
           docs4_desc: "LOAD(MEMORY1)\n　圧縮記憶データ1を読み出します。\nLOAD(MEMORY2)\n　圧縮記憶データ2を読み出します。\nLOAD(MEMORY3)\n　圧縮記憶データ3を読み出します。",
           docs5_title: "削除（DELETE）",
-          docs5_desc: "DELETE(RULE)\n　子守ロボットのルールを削除します。\nDELETE(COMMAND)\n　保持している命令を削除します。",
+          docs5_desc: "DELETE(RULE)\n　子守ロボットのルールを削除します。\nDELETE(COMMAND)\n　保持している命令を削除します。\nDELETE(ALL)\n　すべてのデータを削除します。",
           docs100_title: "うまくいかないときは？",
           docs100_desc: "CHECK()のようにすべて大文字で、最後にかっこをつけてください。\n迷ったら、このドキュメントの文字列をコピー&ペーストしてください。",
         }
