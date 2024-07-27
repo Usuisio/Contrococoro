bd2fc5cd45740f597a8e7f2243fb27f0db5f26dc
ususio <steppingwinds@gmail.com>
2024-07-27 22:00:26 +0900

    Updates

--- assets/index-CYqjkYAr.js
+++ assets/index-CYqjkYAr.js
@@ -298,10 +298,6 @@ DELETE(COMMAND)
   z-index: 10;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   transform: translateX(-50%);
-  &::before {
-    content: "警告";
-    color: yellow;
-  }
 `,d$=al`
   from {
     transform: translateX(-50%) scale(0);
